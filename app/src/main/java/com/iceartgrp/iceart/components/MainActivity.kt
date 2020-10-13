a66fb3643a95f8db0808cb1a892870471273b57f
Steinar Ágúst Steinarsson <steinar.a.steinarsson@landsbankinn.is>
2020-10-13 20:57:02 +0000

    koma mér af stað

--- app/src/main/java/com/iceartgrp/iceart/components/MainActivity.kt
+++ app/src/main/java/com/iceartgrp/iceart/components/MainActivity.kt
@@ -3,14 +3,38 @@ package com.iceartgrp.iceart.components
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.iceartgrp.iceart.R
 import com.iceartgrp.iceart.network.ApiConsumer
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val mOnNavigationSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        item -> when(item.itemId) {
+            R.id.navigation_camera -> {
+                true
+            }
+            R.id.navigation_search -> {
+                val fragmentManager = supportFragmentManager
+                val fragmentTransaction = fragmentManager.beginTransaction()
+                true
+            }
+            R.id.navigation_discover -> {
+                true
+            }
+            R.id.navigation_liked -> {
+                true
+            }
+            else -> false
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // implementation 'com.google.android.material:material:1.2.1'
+        navigationView.setOnNavigationItemSelectedListener(mOnNavigationSelectedListener)
         mainbtn.setOnClickListener {
             ApiConsumer().getPaintingById(
                 0,
