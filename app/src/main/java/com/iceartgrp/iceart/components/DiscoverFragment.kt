6fce2e26d1ec6504b6a287838033ea3e4e5c3a7e
stenesig <steinar1996@gmail.com>
2020-10-20 17:38:54 +0000

    added google maps map

--- app/src/main/java/com/iceartgrp/iceart/components/DiscoverFragment.kt
+++ app/src/main/java/com/iceartgrp/iceart/components/DiscoverFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.GoogleMap
 import com.iceartgrp.iceart.R
+import com.google.android.gms.maps.MapFragment
+import com.google.android.gms.maps.OnMapReadyCallback
+import kotlinx.android.synthetic.main.fragment_discover.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,17 +21,23 @@ private const val ARG_PARAM2 = "param2"
  * Use the [DiscoverFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class DiscoverFragment : Fragment() {
+class DiscoverFragment : Fragment(), OnMapReadyCallback {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var googleMap: GoogleMap
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        map_view.onCreate(savedInstanceState)
+        map_view.onResume()
+
+        map_view.getMapAsync(this)
     }
 
     override fun onCreateView(
@@ -56,5 +66,9 @@ class DiscoverFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        map?.let { googleMap = it}
     }
 }
