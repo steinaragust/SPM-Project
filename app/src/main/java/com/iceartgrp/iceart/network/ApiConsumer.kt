113b8092e0f0a4aa160279708513bb3087eb5e1c
Steinar Ágúst Steinarsson <steinar.a.steinarsson@landsbankinn.is>
2020-11-06 23:41:40 +0000

    lint

--- app/src/main/java/com/iceartgrp/iceart/network/ApiConsumer.kt
+++ app/src/main/java/com/iceartgrp/iceart/network/ApiConsumer.kt
@@ -3,12 +3,12 @@ package com.iceartgrp.iceart.network
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.result.Result
-import com.iceartgrp.iceart.models.Painting
 import com.iceartgrp.iceart.models.Artist
-import com.iceartgrp.iceart.models.artistFromJson
-import com.iceartgrp.iceart.models.paintingFromJson
-import com.iceartgrp.iceart.models.exhibitionsFromJson
 import com.iceartgrp.iceart.models.Exhibition
+import com.iceartgrp.iceart.models.Painting
+import com.iceartgrp.iceart.models.artistFromJson
+import com.iceartgrp.iceart.models.exhibitionsFromJson
+import com.iceartgrp.iceart.models.paintingFromJson
 
 class ApiConsumer {
     companion object {
